97a1f45cb54dd51b813ea384a8117298e253fd0c
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-01-31 22:19:13 +0000

    migrate signal data too

--- test/testdataset.jl
+++ test/testdataset.jl
@@ -3,8 +3,8 @@ using Legolas: @schema, @version
 using Onda
 
 # pointers to test dataset tables + necessary schemas
-const signals_path = S3Path("s3://beacon-public-oss/ondabatches-ci/test-data/clean-sleep/test.onda.signal.arrow?versionId=fzwl6dnDY3UmGUk9y16WXfBmGzLKE6mv")
-const uncompressed_signals_path = S3Path("s3://beacon-public-oss/ondabatches-ci/test-data/clean-sleep/uncompressed.test.onda.signal.arrow?versionId=9vRsgqrY494V4WHvnWloNbAjk_XqyakU")
+const signals_path = S3Path("s3://beacon-public-oss/ondabatches-ci/test-data/clean-sleep/test.onda.signal.arrow?versionId=BCcLcHSQPE.RXFstxDcYciWotittBEGE")
+const uncompressed_signals_path = S3Path("s3://beacon-public-oss/ondabatches-ci/test-data/clean-sleep/uncompressed.test.onda.signal.arrow?versionId=0eW58zErHNUUjkExSLqpCv24UDwnBDTV")
 const stages_path = S3Path("s3://beacon-public-oss/ondabatches-ci/test-data/clean-sleep/test.clean-sleep.sleepstage.arrow?versionId=FiRWymDsbNbeUDFeyWgLmtY8rBPzKTeN")
 
 @schema "sleep-stage" SleepStage
