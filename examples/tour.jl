dfc1200366fe494ad77f22d5a20162428d880ad7
Glenn Moynihan <glennmoy@gmail.com>
2023-04-26 18:24:30 +0000

    se testdata

--- examples/tour.jl
+++ examples/tour.jl
@@ -24,26 +24,30 @@
 #
 # OndaBatches.jl aims to serve these needs, which we will explore in detail below.
 
-# For this walkthrough we will use CleanSleepUtils.jl as a source of data:
-# https://github.com/beacon-biosignals/data-map/tree/main/datasets/clean-sleep/CleanSleepUtils.jl
-# However, you can use whichever source is convenient and accessible to you.
-using CleanSleepUtils
+# For this walkthrough we will use testdataset.jl script as a source of data.
+# You may not have access to this data so use whichever source is convenient if you want to
+# work through this code interactively.
+using DataFrames
 using Dates
 using Distributed
-using Onda
+using Legolas
 using OndaBatches
+using Onda
 using Random
 using Test
 using TimeSpans
 
-# Encodings taken from SleepLab
-const STAGE_ENCODING = Dict(s => UInt8(i) for (i, s) in enumerate(CleanSleepUtils.VALID_STAGES))
 const RNG = MersenneTwister(1)
+const VALID_STAGES = ("wake", "nrem1", "nrem2", "nrem3", "rem", "no_stage")
+const SLEEP_STAGE_INDEX = Dict(s => UInt8(i)
+                               for (i, s)
+                               in enumerate(VALID_STAGES))
 
 # Load the necessary signals and annotations tables
-# Note: you'll need to use a role that has access to the clean-sleep S3 buckets.
-signals = CleanSleepUtils.load_signals()
-annotations = CleanSleepUtils.load_stages()
+include("test/testdataset.jl")
+
+signals = DataFrame(Legolas.read(uncompressed_signals_path))
+annotations = DataFrame(Legolas.read(stages_path))
 
 ###
 ### LabeledSignals
@@ -56,26 +60,18 @@ annotations = CleanSleepUtils.load_stages()
 # This constraint is a legacy of the origns of this code and may not be applicable to all
 # use-cases.
 
-# We will run into an OOM if we try to work with the full dataset.
-# So let's work with a subset so that this can run locally
-const N_TARGETS = 3
-targets = rand(signals.recording, N_TARGETS)
-signals = view(signals, findall(in(targets), signals.recording), :)
-annotations = view(annotations, findall(in(targets), annotations.recording), :)
+# Preprocess the annotations table
+annotations = sort_and_trim_spans(annotations, :recording; epoch=Second(30))
 
 # This discards unnecessary fields such as :author_X, :source_X, and :display_icon_shortcode.
 # Also removes :stage in favour of adding :labels, which get encoded as above and stored as
 # onda.samples, and :label_span.
-labeled_signals = label_signals(
-    signals,
-    annotations;
-    labels_column=:stage,
-    epoch=Second(1),
-    encoding=STAGE_ENCODING,
-    roundto=Second
-)
+labeled_signals = label_signals(signals,
+                                annotations,
+                                labels_column=:stage,
+                                encoding=SLEEP_STAGE_INDEX,
+                                epoch=Second(30))
 
-@test size(labeled_signals, 1) == N_TARGETS  # one for each target recording
 @test eltype(labeled_signals.labels) <: Onda.Samples
 @test eltype(labeled_signals.label_span) <: Vector{TimeSpan}
 
@@ -109,9 +105,9 @@ s1, l1 = load_labeled_signal(labeled_signals[1, :])
 # In this example we are specifying:
 # - 1 channel per sample
 # - 3 samples per batch
-# - Each sample taken over a 2 second window
+# - Each sample taken over a 60 second window
 # - All signals and labels uniformly weighted
-batches = RandomBatches(labeled_signals, nothing, nothing, 1, 3, Second(2))
+batches = RandomBatches(labeled_signals, nothing, nothing, 1, 3, Second(60))
 
 # We can now draw down batches of samples from the RandomBatches
 # Calling iterate_batch returns a batch item and the new state of the RNG after iterating once.
@@ -121,15 +117,16 @@ item, new_state = iterate_batch_item(batches, RNG)
 
 # Here, we return and load a single batch item
 # Note that the labels are not necessarily sampled at the same resolution as the signal.
-# This is because XXX
+# This is because the labels are sampled at 0.033 Hz (1 every 30 seconds) while the signal
+# is sampled at 128 Hz.
 x, y = materialize_batch_item(item)
-@test size(x) == (1, 256)  # 1 channel, 2 seconds @ 128Hz resolution
-@test size(y) == (1, 2)    # 1 label, 2 seconds @ 1Hz
+@test size(x) == (1, 7680)  # 1 channel, 60 seconds @ 128Hz resolution
+@test size(y) == (1, 2)    # 2 labels, one for each 30 second segment
 
 # Additionally, we can draw down the entire batch at once
 batch, new_state = iterate_batch(batches, RNG)
 X, Y = materialize_batch(batch)
-@test size(X) == (1, 256, 3)  # we now draw 3 samples and concatenate along the 3rd dimension
+@test size(X) == (1, 7680, 3)  # we now draw 3 samples and concatenate along the 3rd dimension
 @test size(Y) == (1, 2, 3)
 
 # Note that we can continue to draw as many batches as we like by repeatedly passing the
@@ -154,14 +151,16 @@ X2, Y2 = materialize_batch(batch)
 addprocs(4)
 @everywhere begin
     using Pkg
-    Pkg.add("OndaBatches")
     using OndaBatches
 end
 
+# Note that the "Batch Manager" cannot be assigned to the primary "Manager" node (1) because XXX
+batch_workers = workers()
+batch_manager = popfirst!(batch_workers)
+
 # A Batcher governs the allocation of batch processing on a distributed environment.
 # We'll provide the RandomBatcher defined above.
-# Note that the "Batch Manager" cannot be assigned to the primary "Manager" node (1) because XXX
-batcher = Batcher(2, [3, 4, 5], batches; start=false, state=RNG)
+batcher = Batcher(batch_manager, batch_workers, batches; start=false, state=RNG)
 
 # First let's check the initialised batcher hasn't started
 @test get_status(batcher) == :stopped
@@ -174,9 +173,10 @@ start!(batcher, RNG)
 
 # It should now be running and ready to allocated batches across nodes
 @test get_status(batcher) == :running
-@test isready(batcher.channel)
+@test !isready(batcher.channel)
 
-# X, Y are the collection of training / evaluation data above
+# X, Y are the collection of training / evaluation data as above
+# Similarly, we can keep sampling from this by repeatedly passing in the new_state
 (X3, Y3), new_state = take!(batcher, RNG)
 @test size(X3) == size(X)
 @test size(Y3) == size(Y)
